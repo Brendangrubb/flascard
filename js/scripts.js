4bcd510c0d78fa81b602fdd7ece1e647447619cb
Brendan Grubb and Dylan Stackhouse <brendan@>
2017-01-10 15:58:30 -0800

    add js toggle functionality to flashcards

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,123 @@
 $(document).ready(function() {
 
   $("#javascript").click(function(){
-    $("#javascript p").toggle()
+    $("#javascript p").toggle();
 
-  })
+  });
 
+  $("#js-library").click(function(){
+    $("#js-library p").toggle();
 
+  });
 
-})
+  $("#jquery").click(function(){
+    $("#jquery p").toggle();
+
+  });
+
+  $("#functions").click(function(){
+    $("#functions p").toggle();
+
+  });
+
+  $("#methods").click(function(){
+    $("#methods p").toggle();
+
+  });
+
+  $("#chaining-methods").click(function(){
+    $("#chaining-methods p").toggle();
+
+  });
+
+  $("#arguments").click(function(){
+    $("#arguments p").toggle();
+
+  });
+
+  $("#parameters").click(function(){
+    $("#parameters p").toggle();
+
+  });
+
+  $("#return").click(function(){
+    $("#return p").toggle();
+
+  });
+
+  $("#variables").click(function(){
+    $("#variables p").toggle();
+
+  });
+
+  $("#strings").click(function(){
+    $("#strings p").toggle();
+
+  });
+
+  $("#numbers").click(function(){
+    $("#numbers p").toggle();
+
+  });
+
+  $("#booleans").click(function(){
+    $("#booleans p").toggle();
+
+  });
+
+  $("#undefined").click(function(){
+    $("#undefined p").toggle();
+
+  });
+
+  $("#null").click(function(){
+    $("#null p").toggle();
+
+  });
+
+  $("#nan").click(function(){
+    $("#nan p").toggle();
+
+  });
+
+  $("#infinity").click(function(){
+    $("#infinity p").toggle();
+
+  });
+
+  $("#attributes").click(function(){
+    $("#attributes p").toggle();
+
+  });
+
+  $("#operators").click(function(){
+    $("#operators p").toggle();
+
+  });
+
+  $("#assignment-operators").click(function(){
+    $("#assignment-operators p").toggle();
+
+  });
+
+  $("#comparison-operators").click(function(){
+    $("#comparison-operators p").toggle();
+
+  });
+
+  $("#escape").click(function(){
+    $("#escape p").toggle();
+
+  });
+
+  $("#alert").click(function(){
+    $("#alert p").toggle();
+
+  });
+
+  $("#comments").click(function(){
+    $("#comments p").toggle();
+
+  });
+
+});
